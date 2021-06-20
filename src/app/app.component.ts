f831ca54bb388397e0226dbecb8d5ef5fa8f4f4a
Patricio <67854017+triciopa@users.noreply.github.com>
2021-06-20 17:36:14 -0300

    Input decorator into Sum

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ExerciseSum';
+  numbers: number[];
+
+  constructor() {
+    this.numbers = [1, 8, 45, 954];
+  }
 }
--- src/app/components/sum/sum.component.ts
+++ src/app/components/sum/sum.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sum',
@@ -7,8 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SumComponent implements OnInit {
 
-  num1: number;
-  num2: number;
+  @Input() num1: number;
+  @Input() num2: number;
   result: number;
 
   constructor() {
